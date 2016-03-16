b982d9b4c3d271bd0a0b0d878a490dc9c56532aa
jotask <jota_sk@hotmail.com>
2016-03-16 10:59:59 +0000

    created boths needed classes and the main method

--- src/cs21120/assignment/solution/BTSingleElimjov2.java
+++ src/cs21120/assignment/solution/BTSingleElimjov2.java
@@ -1,9 +1,51 @@
 package cs21120.assignment.solution;
 
-/**
- * @author Jose Vives Iznardo
- * @since 16/03/2016
- */
-public class BTSingleElimjov2 {
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class BTSingleElimjov2 implements IManager{
+
+    @Override
+    public void setPlayers(ArrayList<String> players) {
+
+    }
+
+    @Override
+    public boolean hasNextMatch() {
+        return false;
+    }
+
+    @Override
+    public Match nextMatch() throws NoNextMatchException {
+        return null;
+    }
+
+    @Override
+    public void setMatchScore(int p1, int p2) {
+
+    }
+
+    @Override
+    public String getPosition(int n) {
+        return null;
+    }
+
+    @Override
+    public IBinaryTree getCompetitionTree() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String file = "";
+        CompetitionManager cm = new CompetitionManager(new BTSingleElimjov2());
+        try {
+            cm.runCompetition(file);
+        } catch (FileNotFoundException e) {
+            System.err.println("FILE NOT FOUND");
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
